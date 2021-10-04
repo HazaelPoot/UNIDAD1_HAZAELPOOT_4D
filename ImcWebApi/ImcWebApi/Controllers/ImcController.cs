1f0375c07493eb4260d276cec0e2c242a499df13
HazaelPoot <91893556+HazaelPoot@users.noreply.github.com>
2021-10-04 08:48:51 -0500

    Add files via upload

---
+++ ImcWebApi/ImcWebApi/Controllers/ImcController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Http;
+
+namespace ImcApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ImcController : ControllerBase
+    {
+        [HttpGet("{Peso}/{Altura}")]
+        public string Get(double peso, double altura)
+        {
+            Imcs oimc = new Imcs();
+
+            oimc.peso = peso;
+            oimc.altura = altura;
+            oimc.operacion = (peso / (altura * altura));
+
+            if (oimc.operacion > 25 && oimc.operacion < 29.9)
+            { 
+                oimc.result = "Indice de masa corporal Alto"; 
+            }
+            else if (oimc.operacion > 18.5 && oimc.operacion < 24.9)
+            {
+                oimc.result = "Indice de masa corporal Normal"; 
+            }
+            else if (oimc.operacion < 18.5)
+            {
+                oimc.result = "Indice de masa corporal Bajo"; 
+            }
+            else
+            {
+                oimc.result = "Indice de masa corporal Fuera del rango"; 
+            }
+
+            return $"El Indice de Masa Corporal es de: {Math.Round(oimc.operacion, 2)} : {oimc.result}";
+        }
+    }
+}
---
+++ ImcWebApi/ImcWebApi/Imc.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ImcApi.Controllers
+{
+    public class Imcs
+    {
+        public double peso
+        { get; set; }
+
+        public double altura
+        { get; set; }
+
+        public double operacion
+        { get; set; }
+
+        public string result
+        { get; set; }
+    }
+}
